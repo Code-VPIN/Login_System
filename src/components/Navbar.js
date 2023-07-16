69731f4ef22fba5790f79772b88c9a9d4a342ff7
Vipin Saroj <95955034+Code-VPIN@users.noreply.github.com>
2023-07-16 11:36:23 +0530

    initial setup push

---
+++ src/components/Navbar.js
@@ -0,0 +1,82 @@
+import React from "react";
+import log from "./log.png";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar bg-body-tertiary">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">
+        <img src="" alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
+        Bootstrap
+      </a>
+    </div>
+  </nav>
+
+    // <nav className="navbar">
+    //   <div className="container">
+    //     <a className="navbar-brand" href="#">
+    //       <img
+    //         src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+    //         height="20"
+    //         alt="TAS
+    //         Logo"
+    //         loading="lazy"
+    //       />
+    //     </a>
+
+    //     <div className="d-flex justify-content-center dropdown">
+    //       <button className="btn btn-secondary dropdown-toggle m-2" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    //         Dropdown button
+    //       </button>
+    //       <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    //         <a className="dropdown-item" href="#">Action</a>
+    //         <a className="dropdown-item" href="#">Another action</a>
+    //         <a className="dropdown-item" href="#">Something else here</a>
+    //       </ul>
+    //       <button className="btn btn-secondary dropdown-toggle m-2" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    //         Dropdown button
+    //       </button>
+    //       <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    //         <a className="dropdown-item" href="#">Action</a>
+    //         <a className="dropdown-item" href="#">Another action</a>
+    //         <a className="dropdown-item" href="#">Something else here</a>
+    //       </ul>
+    //       <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    //         <a className="dropdown-item" href="#">
+    //           Action
+    //         </a>
+    //         <a className="dropdown-item" href="#">
+    //           Another action
+    //         </a>
+    //         <a className="dropdown-item" href="#">
+    //           Something else here
+    //         </a>
+    //       </div>
+    //       <div class="p-2 m-2">Flex item 2</div>
+    //       <div class="p-2 m-2">Flex item 3</div>
+
+
+    //       <button className="btn btn-secondary dropdown-toggle m-2" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    //         Dropdown button
+    //       </button>
+    //       <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    //         <a className="dropdown-item" href="#">Action</a>
+    //         <a className="dropdown-item" href="#">Another action</a>
+    //         <a className="dropdown-item" href="#">Something else here</a>
+    // //       </ul>
+ 
+    //         <div className="dropdown mt-3">
+    //             <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownmenu" data-bs-toggle="dropdown">Demo</button>
+    //             <ul className="dropdown-menu" aria-labelledby="dropdownmenu">
+    //                 <li><a href="#" className="dropdown-item">ghutana</a></li>
+    //                 <li><a href="#" className="dropdown-item">ghutana</a></li>
+    //                 <li><a href="#" className="dropdown-item">ghutana</a></li>
+    //                 <li><a href="#" className="dropdown-item">ghutana</a></li>
+    //             </ul>
+    //         </div>
+
+    //     </div>
+    //   </div>
+    // </nav>
+  );
+}
